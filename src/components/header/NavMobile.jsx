c5f2251c8259b853fc1907ee2684662411bf30cb
Krpardo27 <kevin.pardov26@gmail.com>
2025-06-01 15:36:58 -0400

    cambios nuevos

---
+++ src/components/header/NavMobile.jsx
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+
+const NavMobile = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <nav>
+      <ul className="flex gap-4">
+        <li>
+          <Link href="/">Home</Link>
+        </li>
+        <li>
+          <Link href="/about">About</Link>
+        </li>
+        <li>
+          <Link href="/contact">Contact</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default NavMobile;
